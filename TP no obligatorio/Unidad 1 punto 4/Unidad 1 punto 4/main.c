3fcfae62df23294fc820ff2e8e64f33b49cab0d0
LaggerP <pablo_lagger@hotmail.com>
2018-03-21 11:25:07 -0300

    Tercer commit
    Agregado codigo punto 4 unidad 1

---
+++ TP no obligatorio/Unidad 1 punto 4/Unidad 1 punto 4/main.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define b100 100
+#define b50 50
+#define b10 10
+#define b5 5
+#define b1 1
+
+
+int calculador_de_dinero(int dineroEntregar);
+
+int main(void) {
+	int dineroEntregar = 0;
+	printf("Ingrese la cantidad de dinero que desea retirar (0 para finalizar): \n");
+	scanf("%d", &dineroEntregar);
+	int calculador_de_dinero(dineroEntregar);
+}
+
+int calculador_de_dinero(int dineroEntregar) {
+	if (dineroEntregar != 0) {
+		b100 / 100;
+		if()
+	}
+}
